8684ab01e997172b6c825c246dd80128992a27c2
Brendan Telzrow <telzrowb@gmail.com>
2015-07-07 16:04:16 -0500

    Fix counter and percentage function

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+var gamesPlayed = 0;
+var wins = 0;
+var losses = 0;
+
+function counter () {
+	gamesPlayed += 1;
+	document.getElementById("counter").innerHTML = ("Games Played: " + gamesPlayed);
+};
+
 // Random dice roll function
 var rollDice = function () {
 	return Math.floor(Math.random() * 6 + 1);
-}
+};
 
 // Win function
 var win = function () {
 	numberOfWins += 1;
 	document.getElementById("wins").innerHTML = ("Wins: " + numberOfWins);
-}
-
-var wins = 0;
-var losses = 0;
-var percent = 0;
+};
 
 // user selection of 1-6 or random number
 function setNumber (x) {
@@ -25,16 +30,9 @@ function setNumber (x) {
 		document.getElementById("result").innerHTML = ("Sorry, please try again.");
 		losses += 1;
 	}
+	var percent = ((wins/gamesPlayed) * 100).toFixed(2);
 	document.getElementById("winloss").innerHTML = ("Wins / Losses: " + wins + " / " + losses);
-	document.getElementById("percent").innerHTML = ("Win Percentage: " + wins/gamesPlayed * 100 + "%");
-};
-
-// Counts number of games played
-var gamesPlayed = 0;
-
-var counter = function () {
-	gamesPlayed += 1;
-	document.getElementById("counter").innerHTML = ("Games Played: " + gamesPlayed);
+	document.getElementById("percent").innerHTML = ("Win Percentage: " + percent + "%");
 };
 
 
